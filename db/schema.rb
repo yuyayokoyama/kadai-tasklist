be2e9073dbab24291de25863133adcbd8fdd75a0
yuya.yokoyama <potentialability1031@gmail.com>
2019-10-02 08:18:10 +0000

    status-tasklist

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_30_112911) do
+ActiveRecord::Schema.define(version: 2019_10_01_145237) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
