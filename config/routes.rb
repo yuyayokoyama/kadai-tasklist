5434abefdd3a5d527647e406409a6f92812b6d1e
yuya.yokoyama <potentialability1031@gmail.com>
2019-10-10 02:52:46 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
 
-  root to: 'tasks#index'
   root to: 'toppages#index'
 
-  resources :tasks
+  get 'signup', to: 'users#new'
+
+  resources :users, only: [:index, :show, :new, :create]
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_145237) do
+ActiveRecord::Schema.define(version: 2019_10_09_121827) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
